cdee14fa7883943b68ba55d7265f1e5e90c1af91
DaniLopez23 <Daniel.lopez46@alu.uclm.es>
2023-08-28 15:17:32 +0200

    exercises 1.3-1.5

--- part1/src/index.js
+++ part1/src/index.js
@@ -8,18 +8,19 @@ const Header = (props) => {
     </>
   )
 }
+
 //Exercise 1.1
 const Content = (props) => {
   return (
     <>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <p>{props.parts[0].name} {props.parts[0].exercises}</p>
+      <p>{props.parts[1].name} {props.parts[1].exercises}</p>
+      <p>{props.parts[2].name} {props.parts[2].exercises}</p>
     </>
   )
 }
 
-//exercise 1.2
+//Exercise 1.2
 
 // const Part = (props) => {
 //   <p>{props.part} {props.exercise}</p> 
@@ -45,22 +46,38 @@ const Total = (props) => {
 
 
 const App = () => {
-  // const-definitions
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
+    // <>
+    //   <Header course={course} />
+    //   <Content part1={part1.name} part2={part2.name} part3={part3.name}/>
+    //   <Total total={part1.exercises+part2.exercises+part3.exercises} />
+    // </>
     <div>
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3}/>
-      <Total total={exercises1+exercises2+exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
