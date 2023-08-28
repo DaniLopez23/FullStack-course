6fab8c86d950130976cd7d1cb4f0700c103f8c2a
DaniLopez23 <Daniel.lopez46@alu.uclm.es>
2023-08-28 13:32:59 +0200

    exercise 1.1 and 1.2

---
+++ part1/src/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+//Exercise 1.1
+const Content = (props) => {
+  return (
+    <>
+      <p>{props.part1} {props.exercises1}</p>
+      <p>{props.part2} {props.exercises2}</p>
+      <p>{props.part3} {props.exercises3}</p>
+    </>
+  )
+}
+
+//exercise 1.2
+
+// const Part = (props) => {
+//   <p>{props.part} {props.exercise}</p> 
+// }
+
+// const Content = (props) => {
+//   return (
+//     <>
+//       <Part part={props.part1} exercise={props.exercises1}/>
+//       <Part part={props.part2} exercise={props.exercises2}/>
+//       <Part part={props.part3} exercise={props.exercises3}/>
+//     </>
+//   )
+// }
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.total}</p>
+    </>
+  )
+}
+
+
+const App = () => {
+  // const-definitions
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3}/>
+      <Total total={exercises1+exercises2+exercises3} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
